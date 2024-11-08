2236400bd5d6b69daf4841ea04517bd97cf93cf7
Paloma Origuela <106692537+plomxv@users.noreply.github.com>
2024-11-08 13:27:21 -0300

    Add files via upload

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,4 +1,4 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = 'https://raw.githubusercontent.com/pmatjayme/api/refs/heads/main/paloma.json'
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
