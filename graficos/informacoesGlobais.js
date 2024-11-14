d964c274504a82d17096c360e45081724ffd0c23
Paloma Origuela <106692537+plomxv@users.noreply.github.com>
2024-11-14 15:38:19 -0300

    iiiiip

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,7 +6,7 @@ async function vizualizarInformacoesGlobais() {
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = 'Você sabia que no 3B tem <span>${dados.Solteiro} pessoas solteiras,</span> <span>${Namorando} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${Enrolado} horas</span> e <span>${Fundo do Poço} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${Casado}%</span> de pessoas estão conectadas em alguma rede social.'
+    paragrafo.innerHTML = 'Você sabia que no 3B tem <span> ${dados.Solteiro} pessoas solteiras,</span> <span> ${Namorando} bilhões</span> estão conectadas em alguma rede social e passam em média <span> ${Enrolado} horas</span> e <span> ${Fundo do Poço} minutos</span> conectadas.<br>Isso significa que aproximadamente <span> ${Casado}% </span> de pessoas estão conectadas em alguma rede social.'
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
