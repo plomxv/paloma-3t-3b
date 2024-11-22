f378ff5e1b9535a1ae2edef085d5951acf7a185f
Paloma Origuela <106692537+plomxv@users.noreply.github.com>
2024-11-22 13:24:38 -0300

    fim

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,8 +6,7 @@ async function vizualizarInformacoesGlobais() {
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.Solteiro}</span> bilhões de pessoas e que aproximadamente <span>${dados.Namorando}</span> bilhões estão
-    empregadas, sendo a taxa de desemprego de <span>${dados.Enrolado}</span> %. Por fim, por semana, trabalham em média <span>${dados.Fundo_poco}</span> horas e <span>${dados.Casado}</span> minutos.`
+    paragrafo.innerHTML = `Em nossa sala, temos <span>${dados.Solteiro}</span> solteiros, enquanto que <span>${dados.Namorando}</span> estão namorando. Há <span>${dados.Enrolado}</span> enrolados. Por fim, junto ao Luiz, temos <span>${dados.Fundo_poco}</span> no fundo do poço e <span>${dados.Casado}</span> casados.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
 }
